ae998aff713bff1e6ab3b87a289519e19b401d70
zouzzzzzzzzzzzz <148040761+zouzzzzzzzzzzzz@users.noreply.github.com>
2023-10-16 09:07:57 +0200

    Update SimpleTesting.java
    code java

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,1 +1,8 @@
+public class SimpleTesting {
+public static void main(String[] args) {
+Student student = new Student("John");
+String name = student.getName();
+System.out.println("Student name is : "+name);
+}
+}
 
